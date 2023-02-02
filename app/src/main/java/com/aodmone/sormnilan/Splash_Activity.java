a40a0cd474d40945ea44e4d996fe366ff1639427
div79 <blackbotinfotech@gmail.com>
2023-02-02 12:02:59 +0530

    version2.0.00

--- app/src/main/java/com/aodmone/sormnilan/Splash_Activity.java
+++ app/src/main/java/com/aodmone/sormnilan/Splash_Activity.java
@@ -1,11 +1,12 @@
 package com.aodmone.sormnilan;
 
-import static com.jesdene.jesdenias.MACT.getRetryBtn;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 
 
 public class Splash_Activity extends AppCompatActivity {
